a70b846fa153d743713e7de63acead597485f11d
Pranesh_SJ <727721euit112@skcet.ac.in>
2023-07-11 14:54:18 +0530

    Add files via upload

---
+++ Day_4/Registration.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Registration() {
+  return (
+    <>
+      <header>
+      <h2 class ="logo">Logo</h2>
+      <nav class="navigation">
+        <a href="/">Home</a>
+        <a href="b">About</a>
+        <a href="c">Service</a>
+        <a href="d">Contact</a>
+        <button class="btnLogin-popup" href="/">
+        <Link to="/login">LOGIN</Link>
+        </button>
+      </nav>
+    </header> 
+    <div class="wrapper">
+    <div class ="form-box login">
+      <h2>Register</h2>
+      <form action="a">
+        <div class="input-box">
+          <span class="icon"></span>
+          <input type="string" required/>
+          <label>Name</label>
+        </div>
+        <div class="input-box">
+          <span class="icon"></span>
+          <input type="email" required/>
+          <label>Email</label>
+        </div>
+        <div class="input-box">
+          <span class="icon"></span>
+          <input type="password" required/>
+          <label>Set Password</label>
+        </div>
+        <div class="remember-forgot">
+          <label><input type="checkbox"/>Accept to all condition</label>
+          <a href="gf">Already have a account</a>
+        </div>
+        <button type="submit" class="btn">Register</button>
+        
+      </form>
+    </div>
+    </div>
+    
+    </>
+  )
+}
